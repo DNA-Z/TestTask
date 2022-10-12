4958eed445ae64cf6f3481f121c4a018f828a2e2
Inna Borodina <creator.geek@gmail.com>
2022-10-13 00:32:01 +0300

    Added a class FileParser to read a file and get dictionary

---
+++ TestTask.Application/FileParser.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Reflection.PortableExecutable;
+
+namespace TestTask.Application
+{
+    public class FileParser
+    {
+
+        /// <summary>
+        /// Parses the file and returns a dictionary, where the key is the file path and the value is the checksum
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> Parse(string path)
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', ';', '\t', '\n' };
+            string fileContent = ReadFile(path);
+            string[] fileContentParse = fileContent.Split(delimiterChars);
+            var fileChecksums = fileContentParse.Select(s => s.Split(delimiterChars))
+                .ToDictionary(arr => arr[0], arr => arr[1]);
+
+            return fileChecksums;
+        }
+
+        public string ReadFile(string path)
+        {
+            string fileContent = string.Empty;
+
+            using(var reader = new StreamReader(path))
+            {
+                do
+                {
+                    fileContent = reader.ReadLine();
+                } while (fileContent != null);
+            }
+
+            return fileContent;
+        }
+
+
+    }
+}
